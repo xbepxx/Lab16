d2d77da9f680d5a15b1ca4af290e3bc58af07b67
xbepxx@gmail.com <xbepxx@gmail.com>
2021-10-19 13:10:39 +0300

    Добавьте файлы проекта.

---
+++ Lab16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Lab16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = "{\"firstName\":\"Tom\",\"lastName\":\"Jackson\",\"gender\":\"male\",\"age\":29,\"online\":true,\"hobby\":[\"football\", \"reading\", \"swimming\"]}";
+            Person person = JsonSerializer.Deserialize<Person>(jsonString);
+        }
+    }
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public bool online { get; set; }
+        public string hobby { get; set; }
+    }
+}
